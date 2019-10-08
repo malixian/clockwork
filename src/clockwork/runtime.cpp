1416172864e821aec1f2eb9fcbf98f788baa3e1a
Jonathan Mace <jcmace@mpi-sws.org>
2019-10-08 17:30:53 +0000

    Split out some of the code in action into runtime

---
+++ src/clockwork/runtime.cpp
@@ -0,0 +1,91 @@
+#include "clockwork/runtime.h"
+
+namespace clockwork {
+
+Executor::Executor(TaskType type) : alive(true), type(type){
+	unsigned numThreads = 1;
+	for (unsigned i = 0; i < numThreads; i++) {
+		threads.push_back(std::thread(&Executor::executorMain, this, i));
+	}
+}
+
+void Executor::enqueue(Task* task) {
+	queue.enqueue(task, task->eligible());
+}
+
+void Executor::shutdown() {
+	alive.store(false);
+	queue.shutdown();
+}
+
+void Executor::join() {
+	for (unsigned i = 0; i < threads.size(); i++) {
+		threads[i].join();
+	}
+}
+
+void Executor::executorMain(int executorId) {
+	// TODO: acquire core, bind to core, set thread priority
+	util::initializeCudaStream();
+	cudaStream_t stream = util::Stream();
+
+	while (alive.load()) {
+		// TODO: possibility off too many outstanding asyc tasks
+
+		// TODO: queue should spin-wait rather than blocking
+		// TODO: shutdown queue or use try_dequeue
+		Task* next = queue.dequeue();
+
+		next->telemetry->dequeued = util::hrt();
+		next->run(stream);
+		next->telemetry->exec_complete = util::hrt();
+	}
+}
+
+
+AsyncTaskChecker::AsyncTaskChecker() : alive(true) {
+	unsigned num_threads = 1;
+	for (unsigned i = 0; i < num_threads; i++) {
+		threads.push_back(std::thread(&AsyncTaskChecker::executorMain, this, i));
+	}
+}
+
+void AsyncTaskChecker::enqueue(AsyncTask* task) {
+	queue.push(task);
+}
+
+void AsyncTaskChecker::shutdown() {
+	alive.store(false);
+	// TODO: notify queue waiters
+}
+
+void AsyncTaskChecker::join() {
+	for (unsigned i = 0; i < threads.size(); i++) {
+		threads[i].join();
+	}
+}
+
+void AsyncTaskChecker::executorMain(int executorId) {
+	// TODO: acquire core, bind to core, set thread priority
+	util::initializeCudaStream();
+
+	std::vector<AsyncTask*> pending_tasks;
+	while (alive.load()) {
+		AsyncTask* next;
+		while (queue.try_pop(next)) {
+			pending_tasks.push_back(next);
+		}
+
+		std::vector<AsyncTask*> still_pending;
+		for (AsyncTask* task : pending_tasks) {
+			if (task->is_complete()) {
+				task->process_completion();
+			} else {
+				still_pending.push_back(task);
+			}
+		}
+		pending_tasks = still_pending;
+	}
+}
+
+}
---
+++ src/clockwork/runtime.h
@@ -0,0 +1,90 @@
+#ifndef _CLOCKWORK_RUNTIME_H_
+#define _CLOCKWORK_RUNTIME_H_
+
+#include <thread>
+#include <limits>
+#include <algorithm>
+#include <memory>
+#include <atomic>
+#include <cuda_runtime.h>
+#include "tvm/runtime/cuda_common.h"
+#include "clockwork/telemetry.h"
+#include "clockwork/cache.h"
+#include "clockwork/model/model.h"
+#include "clockwork/priority_queue.h"
+#include "clockwork/common.h"
+#include "tbb/concurrent_queue.h"
+#include "clockwork/task.h"
+
+namespace clockwork {
+
+class Executor {
+private:
+	std::atomic_bool alive;
+	time_release_priority_queue<Task> queue;
+	std::vector<std::thread> threads;
+
+public:
+	const TaskType type;
+
+	Executor(TaskType type);
+
+	void enqueue(Task* task);
+	void shutdown();
+	void join();
+	void executorMain(int executorId);
+};
+
+class AsyncTaskChecker {
+private:
+	std::atomic_bool alive;
+	tbb::concurrent_queue<AsyncTask*> queue;
+	std::vector<std::thread> threads;
+
+public:
+
+	AsyncTaskChecker();
+
+	void enqueue(AsyncTask* task);
+	void shutdown();
+	void join();
+	void executorMain(int executorId);
+};
+
+class ClockworkRuntime {
+public:
+	MemoryManager* manager;
+
+	Executor* weights_executor;
+	Executor* inputs_executor;
+	Executor* gpu_executor;
+	Executor* outputs_executor;
+
+	AsyncTaskChecker* checker;
+
+	ClockworkRuntime() {
+		// TODO: factor this out; for now hard-coded 8GB cache w/ 16MB pages
+		int page_size = 16 * 1024 * 1024;
+		int cache_size = 50 * page_size;
+		void* baseptr;
+    	CUDA_CALL(cudaMalloc(&baseptr, cache_size));
+    	PageCache* cache = new PageCache(static_cast<char*>(baseptr), cache_size, page_size, false);
+
+		manager = new MemoryManager();
+	    manager->weights_cache = cache;
+	    manager->workspace_cache = cache;
+	    manager->models = new ModelStore();
+
+		weights_executor = new Executor(PCIe_H2D_Weights);
+		inputs_executor = new Executor(PCIe_H2D_Inputs);
+		gpu_executor = new Executor(GPU);
+		outputs_executor = new Executor(PCIe_D2H_Output);
+		checker = new AsyncTaskChecker();
+	}
+
+};
+
+
+}
+
+#endif
