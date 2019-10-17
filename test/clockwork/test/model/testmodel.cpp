298ce852bf2cab2a3653478da4bdfff8a9e2010b
Jonathan Mace <jcmace@mpi-sws.org>
2019-10-17 15:54:27 +0000

    Add a test case for a batched model

--- test/clockwork/test/model/testmodel.cpp
+++ test/clockwork/test/model/testmodel.cpp
@@ -200,3 +200,66 @@ TEST_CASE("Model produces correct output", "[e2e]") {
         REQUIRE(actualOutputF[i] == expectedOutputF[i]);
     }
 }
+
+TEST_CASE("Batched model produces correct output", "[e2e2]") {
+
+    int page_size = 16 * 1024 * 1024;
+    int input_size = 2*224*224*3*4;
+    int output_size = 1000 * 2 * 4;
+
+    std::string f = clockwork::util::get_example_model("resnet18_tesla-m40_batchsize2");
+
+    Model* model = Model::loadFromDisk(f+".so", f+".clockwork", f+".clockwork_params");
+    
+    model->instantiate_model_on_host();
+    model->instantiate_model_on_device();
+
+    std::vector<char*> weights_pages = make_cuda_pages(page_size, model->num_weights_pages(page_size));
+    model->transfer_weights_to_device(weights_pages, NULL);
+
+
+
+    std::string input, expectedOutput;
+    clockwork::util::readFileAsString(f+".input", input);
+    clockwork::util::readFileAsString(f+".output", expectedOutput);
+
+    REQUIRE(input_size == 2 * input.size());
+    REQUIRE(output_size == 2 * expectedOutput.size());
+
+    char batched_input[input_size];
+    std::memcpy(batched_input, input.data(), input.size());
+    std::memcpy(batched_input + input.size(), input.data(), input.size());
+
+    char batched_expected_output[output_size];
+    std::memcpy(batched_expected_output, expectedOutput.data(), expectedOutput.size());
+    std::memcpy(batched_expected_output + expectedOutput.size(), expectedOutput.data(), expectedOutput.size());
+
+
+    std::vector<char*> workspace_pages = make_cuda_pages(page_size, model->num_workspace_pages(page_size));
+    model->transfer_input_to_device(batched_input, workspace_pages, NULL);
+    model->call(weights_pages, workspace_pages, NULL);
+
+    char actualOutput[output_size];
+    model->transfer_output_from_device(actualOutput, workspace_pages, NULL);
+
+    cuda_synchronize(NULL);
+
+    float* actualOutputF = static_cast<float*>(static_cast<void*>(actualOutput));
+    float* expectedOutputF = static_cast<float*>(static_cast<void*>(batched_expected_output));
+
+    auto max_index_actual_1 = std::distance(actualOutputF, std::max_element(actualOutputF, actualOutputF + 1000));
+    auto max_index_expect_1 = std::distance(expectedOutputF, std::max_element(expectedOutputF, expectedOutputF + 1000));
+    REQUIRE(max_index_expect_1 == max_index_actual_1);
+
+    std::cout << "max_index_actual_1 is " << max_index_actual_1 << std::endl;
+
+    auto max_index_actual_2 = std::distance(actualOutputF+1000, std::max_element(actualOutputF+1000, actualOutputF + 2000));
+    auto max_index_expect_2 = std::distance(expectedOutputF+1000, std::max_element(expectedOutputF+1000, expectedOutputF + 2000));
+    REQUIRE(max_index_expect_2 == max_index_actual_2);
+
+    std::cout << "max_index_actual_2 is " << max_index_actual_2 << std::endl;
+
+    for (unsigned i = 0; i < output_size/4; i++) {
+        REQUIRE(actualOutputF[i] == expectedOutputF[i]);
+    }
+}
