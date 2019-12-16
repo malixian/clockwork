63237eadd3b942b3d2b6938f66de28fb8b0e34b4
Jonathan Mace <jcmace@mpi-sws.org>
2019-12-16 14:01:08 +0100

    Avoid instantiating workers on heap

--- test/clockwork/test/testworker.cpp
+++ test/clockwork/test/testworker.cpp
@@ -43,138 +43,140 @@ public:
 };
 
 TEST_CASE("Test Worker", "[worker]") {
-    ClockworkWorker* worker = new ClockworkWorker();
-    TestController* controller = new TestController();
-    worker->controller = controller;
+    ClockworkWorker worker;
+    TestController controller;
+    worker.controller = &controller;
 
     auto load_model = load_model_from_disk_action();
 
     std::vector<std::shared_ptr<workerapi::Action>> actions{load_model};
 
-    worker->sendActions(actions);
-    controller->expect(actionSuccess);
+    worker.sendActions(actions);
+    controller.expect(actionSuccess);
 
-    worker->shutdown(true);
-    delete worker;
+    worker.shutdown(true);
+    
 }
 
 TEST_CASE("Test Infer No Weights", "[worker] [noweights]") {
-    ClockworkWorker* worker = new ClockworkWorker();
-    TestController* controller = new TestController();
-    worker->controller = controller;
+    ClockworkWorker worker;
+    TestController controller;
+    worker.controller = &controller;
 
     auto load_model = load_model_from_disk_action();
     std::vector<std::shared_ptr<workerapi::Action>> actions{load_model};
-    worker->sendActions(actions);
-    controller->expect(actionSuccess);
+    worker.sendActions(actions);
+    controller.expect(actionSuccess);
 
-    auto infer = infer_action2(worker);
+    auto infer = infer_action2(&worker);
     actions = {infer};
-    worker->sendActions(actions);
-    controller->expect(actionErrorModelWeightsNotPresent);
+    worker.sendActions(actions);
+    controller.expect(actionErrorModelWeightsNotPresent);
 
-    worker->shutdown(true);
-    delete worker;
+    worker.shutdown(true);
+    
 }
 
-TEST_CASE("Test Infer Weights Not There Yet", "[worker] [noweights]") {
-    ClockworkWorker* worker = new ClockworkWorker();
-    TestController* controller = new TestController();
-    worker->controller = controller;
+// // The below test is commented out because it relies on non-deterministic timing
+//
+// TEST_CASE("Test Infer Weights Not There Yet", "[worker] [noweights]") {
+//     ClockworkWorker worker;
+//     TestController controller;
+//     worker.controller = &controller;
 
-    auto load_model = load_model_from_disk_action();
-    std::vector<std::shared_ptr<workerapi::Action>> actions{load_model};
-    worker->sendActions(actions);
-    controller->expect(actionSuccess);
+//     auto load_model = load_model_from_disk_action();
+//     std::vector<std::shared_ptr<workerapi::Action>> actions{load_model};
+//     worker.sendActions(actions);
+//     controller.expect(actionSuccess);
 
-    auto load_weights = load_weights_action();
-    auto infer = infer_action2(worker);
-    actions = {load_weights, infer};
-    worker->sendActions(actions);
-    controller->expect(actionErrorModelWeightsNotPresent);
-    controller->expect(actionSuccess);
+//     auto load_weights = load_weights_action();
+//     auto infer = infer_action2(&worker);
+//     actions = {load_weights, infer};
+//     worker.sendActions(actions);
+//     controller.expect(actionErrorModelWeightsNotPresent);
+//     controller.expect(actionSuccess);
 
-    worker->shutdown(true);
-    delete worker;
-}
+//     worker.shutdown(true);
+//     
+// }
 
 TEST_CASE("Test Infer Invalid Input", "[worker] [invalid]") {
-    ClockworkWorker* worker = new ClockworkWorker();
-    TestController* controller = new TestController();
-    worker->controller = controller;
+    ClockworkWorker worker;
+    TestController controller;
+    worker.controller = &controller;
 
     auto load_model = load_model_from_disk_action();
     std::vector<std::shared_ptr<workerapi::Action>> actions{load_model};
-    worker->sendActions(actions);
-    controller->expect(actionSuccess);
+    worker.sendActions(actions);
+    controller.expect(actionSuccess);
 
-    auto infer = infer_action2(worker);
+    auto infer = infer_action2(&worker);
     infer->input_size = 10;
     infer->input = nullptr;
     actions = {infer};
-    worker->sendActions(actions);
-    controller->expect(actionErrorInvalidInput);
+    worker.sendActions(actions);
+    controller.expect(actionErrorInvalidInput);
 
-    worker->shutdown(true);
-    delete worker;
+    worker.shutdown(true);
+    
 }
 
 TEST_CASE("Test Infer Invalid Input Size", "[worker] [invalid]") {
-    ClockworkWorker* worker = new ClockworkWorker();
-    TestController* controller = new TestController();
-    worker->controller = controller;
+    ClockworkWorker worker;
+    TestController controller;
+    worker.controller = &controller;
 
     auto load_model = load_model_from_disk_action();
     std::vector<std::shared_ptr<workerapi::Action>> actions{load_model};
-    worker->sendActions(actions);
-    controller->expect(actionSuccess);
+    worker.sendActions(actions);
+    controller.expect(actionSuccess);
 
-    auto infer = infer_action2(worker);
+    auto infer = infer_action2(&worker);
     infer->input_size = 100;
     infer->input = static_cast<char*>(malloc(100));
     actions = {infer};
-    worker->sendActions(actions);
-    controller->expect(actionErrorInvalidInput);
+    worker.sendActions(actions);
+    controller.expect(actionErrorInvalidInput);
 
-    worker->shutdown(true);
-    delete worker;
+    worker.shutdown(true);
+    
 }
 
 TEST_CASE("Test Worker E2E Simple", "[worker] [e2esimple]") {
-    ClockworkWorker* worker = new ClockworkWorker();
-    TestController* controller = new TestController();
-    worker->controller = controller;
+    ClockworkWorker worker;
+    TestController controller;
+    worker.controller = &controller;
 
     std::vector<std::shared_ptr<workerapi::Action>> actions;
 
     auto load_model = load_model_from_disk_action();
     actions = {load_model};
-    worker->sendActions(actions);
-    controller->expect(actionSuccess);
+    worker.sendActions(actions);
+    controller.expect(actionSuccess);
 
     auto load_weights = load_weights_action();
     actions = {load_weights};
-    worker->sendActions(actions);
-    controller->expect(actionSuccess);
+    worker.sendActions(actions);
+    controller.expect(actionSuccess);
 
-    auto infer = infer_action2(worker);
+    auto infer = infer_action2(&worker);
     actions = {infer};
-    worker->sendActions(actions);
-    controller->expect(actionSuccess);
+    worker.sendActions(actions);
+    controller.expect(actionSuccess);
 
     auto evict_weights = evict_weights_action();
     actions = {evict_weights};
-    worker->sendActions(actions);
-    controller->expect(actionSuccess);
+    worker.sendActions(actions);
+    controller.expect(actionSuccess);
 
-    worker->shutdown(true);
-    delete worker;
+    worker.shutdown(true);
+    
 }
 
 TEST_CASE("Test Worker E2E Timed Success", "[worker]") {
-    ClockworkWorker* worker = new ClockworkWorker();
-    TestController* controller = new TestController();
-    worker->controller = controller;
+    ClockworkWorker worker;
+    TestController controller;
+    worker.controller = &controller;
 
     auto load_model = load_model_from_disk_action();
 
@@ -182,7 +184,7 @@ TEST_CASE("Test Worker E2E Timed Success", "[worker]") {
     load_weights->earliest = load_model->earliest + 1000000000UL;
     load_weights->latest = load_weights->earliest + 100000000UL;
 
-    auto infer = infer_action2(worker);
+    auto infer = infer_action2(&worker);
     infer->earliest = load_weights->earliest + 20000000;
     infer->latest = infer->earliest + 100000000UL;
 
@@ -191,12 +193,12 @@ TEST_CASE("Test Worker E2E Timed Success", "[worker]") {
     evict_weights->latest = evict_weights->earliest + 100000000UL;
 
     std::vector<std::shared_ptr<workerapi::Action>> actions{load_model, load_weights, infer, evict_weights};
-    worker->sendActions(actions);
+    worker.sendActions(actions);
 
     for (unsigned i = 0; i < 4; i++) {
-        controller->expect(actionSuccess);
+        controller.expect(actionSuccess);
     }
 
-    worker->shutdown(true);
-    delete worker;
+    worker.shutdown(true);
+    
 }
