0bb10f42100e25dd70d92484c0a3a96550add4ca
Jonathan Mace <jonathan.c.mace@gmail.com>
2020-05-08 21:35:50 +0200

    Add some gurobi hacking

--- src/controller.cpp
+++ src/controller.cpp
@@ -4,7 +4,9 @@
 // #include "clockwork/controller/stress_test_controller.h"
 
 #include "gurobi_c++.h"
-
+#include <cstdlib>
+#include <algorithm>    // std::random_shuffle
+#include <sstream>
 
 // using namespace clockwork;
 
@@ -56,242 +58,288 @@
 
 // 	std::cout << "Clockwork Worker Exiting" << std::endl;
 // }
+/* Copyright 2020, Gurobi Optimization, LLC */
+
+/* This example formulates and solves the following simple MIP model:
+
+     maximize    x +   y + 2 z
+     subject to  x + 2 y + 3 z <= 4
+                 x +   y       >= 1
+                 x, y, z binary
+*/
+
 #include "gurobi_c++.h"
-#include <sstream>
 using namespace std;
 
-int solveAndPrint(GRBModel& model, GRBVar& totSlack,
-                  int nWorkers, string* Workers,
-                  GRBVar* totShifts);
+class Request;
+class Worker;
+
+class RequestOnWorker {
+public:
+  std::string name;
+  Request* req;
+  Worker* worker;
+  GRBVar var;
+  bool ran = false;
+  RequestOnWorker(Request* req, Worker* worker, std::string name) : req(req), worker(worker), name(name) {
+  }
+  void addToModel(GRBModel &model, float size) {
+    var = model.addVar(0.0, 1.0, 1.0, GRB_BINARY);
+    if (ran) var.set(GRB_DoubleAttr_Start, 1.0);
+  }
+  void printIfAssigned(float size) {
+    if (var.get(GRB_DoubleAttr_X) == 1.0) {
+      std::cout << name << " " << size << std::endl;
+    }
+  }
+};
+
+class Request {
+public:
+  unsigned id;
+  float size;
+  float deadline;
+  bool ran = false;
+  std::vector<RequestOnWorker*> instances;
+  Request(unsigned request_id, float size, float deadline) : id(request_id), size(size), deadline(deadline) {}
+  void addToModel(GRBModel &model) {
+    GRBLinExpr expr = 0;
+    for (auto &r : instances) {
+      expr += r->var;
+    }
+    model.addConstr(expr <= 1);
+  }
+};
+
+class Worker {
+public:
+  unsigned id;
+  float available;
+  float sched_available = 0;
+  std::vector<RequestOnWorker*> queue;
+  Worker(unsigned worker_id) : id(worker_id), available(0) {}
+  void addToModel(GRBModel &model) {
+    for (unsigned i = 0; i < queue.size(); i++) {
+      GRBLinExpr expr = available;
+      for (unsigned j = 0; j <= i; j++) {
+        expr += (queue[j]->var * queue[j]->req->size);
+      }
+      std::stringstream s;
+      s << "Execute Request " << queue[i]->req->id << " on Worker " << id;
+      model.addConstr(expr <= (queue[i]->req->deadline + queue[i]->req->size), s.str());
+    }
+  }
+};
+
+class Clockwork {
+public:
+  std::vector<Request*> requests;
+  std::vector<Worker*> workers;
+  std::vector<RequestOnWorker*> instances;
+
+  void addWorker() {
+    workers.push_back(new Worker(workers.size()));
+  }
+
+  void enqueue(float size, float deadline, std::vector<unsigned> workerids) {
+    std::stringstream ss;
+    ss.precision(1);
+    ss << std::fixed;
+    ss << "Add r-" << requests.size() << " size=" << size << " deadline=" << deadline << " on " << workerids[0];
+    for (unsigned i = 1; i < workerids.size(); i++) {
+      ss << "," << workerids[i];
+    }
+    std::cout << ss.str() << std::endl;
+
+    float min_worker_t = deadline-size;
+    Worker* min_worker = nullptr;
+    RequestOnWorker* min_rw = nullptr;
+
+
+    Request* r = new Request(requests.size(), size, deadline);
+    for (unsigned worker_id : workerids) {
+      Worker* w = workers[worker_id];
+
+      std::stringstream s;
+      s << "r-" << r->id << "-w-" << w->id;
+      std::string name = s.str();
+
+      RequestOnWorker* rw = new RequestOnWorker(r, w, name);
+
+      r->instances.push_back(rw);
+      w->queue.push_back(rw);
+      instances.push_back(rw);
+
+      if (w->sched_available <= min_worker_t) {
+        min_worker_t = w->sched_available;
+        min_worker = w;
+        min_rw = rw;
+      }
+    }
+    requests.push_back(r);
+
+    if (min_worker != nullptr) {
+      min_rw->ran = true;
+      r->ran = true;
+      min_worker->sched_available += size;
+    }
+  }
+
+  void addToModel(GRBModel &model) {
+    GRBLinExpr obj = 0;
+    for (RequestOnWorker* rw : instances) {
+      rw->addToModel(model, rw->req->size);
+      obj += rw->var;
+    }
+    for (Request* r : requests) {
+      r->addToModel(model);
+    }
+    for (Worker* w : workers) {
+      w->addToModel(model);
+    }
+    // model.setObjective(obj, GRB_MAXIMIZE);
+  }
+
+  void printAssignments() {
+    for (auto rw : instances) {
+      rw->printIfAssigned(rw->req->size);
+    }
+  }
+
+  void printFifoObjective() {
+    float total = 0;
+    for (Request* r : requests) {
+      if (r->ran) total += r->size;
+    }
+    std::cout << "Fifo objective = " << total << std::endl;
+  }
+};
+
+std::vector<unsigned> assign(unsigned num_workers) {
+  std::vector<bool> loaded(num_workers, false);
+
+  loaded[0] = true;
+  for (unsigned i = 1; i < num_workers; i++) {
+    if (rand() % 2 == 0) break;
+    loaded[i] = true;
+  }
+
+  std::random_shuffle(loaded.begin(), loaded.end());
+  std::vector<unsigned> result;
+  for (unsigned i = 0; i < loaded.size(); i++) {
+    if (loaded[i]) result.push_back(i);
+  }
+  return result;
+}
+
+float myrand() {
+  return static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+}
 
 int
 main(int   argc,
      char *argv[])
 {
-  GRBEnv* env = 0;
-  GRBVar** x = 0;
-  GRBVar* slacks = 0;
-  GRBVar* totShifts = 0;
-  GRBVar* diffShifts = 0;
-  int xCt = 0;
+  try {
+    srand(0);
 
-  try
-  {
-    // Sample data
-    const int nShifts = 14;
-    const int nWorkers = 7;
+    float deadline_min = 20;
+    float deadline_max = 100;
+    float size_min = 1;
+    float size_max = 9;
+    int num_workers = 24;
+    int num_requests = 100;
 
-    // Sets of days and workers
-    string Shifts[] =
-      { "Mon1", "Tue2", "Wed3", "Thu4", "Fri5", "Sat6",
-        "Sun7", "Mon8", "Tue9", "Wed10", "Thu11", "Fri12", "Sat13",
-        "Sun14" };
-    string Workers[] =
-      { "Amy", "Bob", "Cathy", "Dan", "Ed", "Fred", "Gu" };
+    float load = (num_requests * ((size_max - size_min) / 2.0)) / ((deadline_max - deadline_min) * num_workers);
 
-    // Number of workers required for each shift
-    double shiftRequirements[] =
-      { 3, 2, 4, 4, 5, 6, 5, 2, 2, 3, 4, 6, 7, 5 };
+    std::cout << "Adding " << num_requests << " requests on " << num_workers << " workers" << std::endl;
+    std::cout << "Estimated load " << load << std::endl;
 
-    // Worker availability: 0 if the worker is unavailable for a shift
-    double availability[][nShifts] =
-      { { 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1 },
-        { 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0 },
-        { 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1 },
-        { 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1 },
-        { 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1 },
-        { 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1 },
-        { 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 } };
-
-    // Model
-    env = new GRBEnv();
-    GRBModel model = GRBModel(*env);
-    model.set(GRB_StringAttr_ModelName, "assignment");
-
-    // Assignment variables: x[w][s] == 1 if worker w is assigned
-    // to shift s. This is no longer a pure assignment model, so we must
-    // use binary variables.
-    x = new GRBVar*[nWorkers];
-    int s, w;
-
-    for (w = 0; w < nWorkers; ++w) {
-      x[w] = model.addVars(nShifts);
-      xCt++;
-
-      for (s = 0; s < nShifts; ++s) {
-        ostringstream vname;
-
-        vname << Workers[w] << "." << Shifts[s];
-        x[w][s].set(GRB_DoubleAttr_UB, availability[w][s]);
-        x[w][s].set(GRB_CharAttr_VType, GRB_BINARY);
-        x[w][s].set(GRB_StringAttr_VarName, vname.str());
-      }
+    Clockwork clockwork;
+    for (unsigned i = 0; i < num_workers; i++) {
+      clockwork.addWorker();
     }
 
-    // Slack variables for each shift constraint so that the shifts can
-    // be satisfied
-    slacks = model.addVars(nShifts);
-    for (s = 0; s < nShifts; ++s) {
-      ostringstream vname;
-
-      vname << Shifts[s] << "Slack";
-      slacks[s].set(GRB_StringAttr_VarName, vname.str());
+    for (unsigned i = 0; i < num_workers; i++) {
+      clockwork.enqueue(80, 81, {i});
+    }
+    for (unsigned i = 0; i < num_requests; i++) {
+      std::vector<unsigned> workerids = assign(clockwork.workers.size());
+      float deadline = deadline_min + i * (deadline_max - deadline_min) / num_requests;
+      float size = size_min + myrand() * (size_max - size_min);
+      clockwork.enqueue(size, deadline, workerids);
     }
 
-    // Variable to represent the total slack
-    GRBVar totSlack = model.addVar(0, GRB_INFINITY, 0, GRB_CONTINUOUS,
-                                   "totSlack");
 
-    // Variables to count the total shifts worked by each worker
-    totShifts = model.addVars(nWorkers);
-    for (w = 0; w < nWorkers; ++w) {
-      ostringstream vname;
+    // Create an environment
+    GRBEnv env = GRBEnv(true);
+    // env.set("LogFile", "mip1.log");
+    env.start();
 
-      vname << Workers[w] << "TotShifts";
-      totShifts[w].set(GRB_StringAttr_VarName, vname.str());
-    }
+    // Create an empty model
+    GRBModel model = GRBModel(env);
 
-    GRBLinExpr lhs;
+    clockwork.addToModel(model);
 
-    // Constraint: assign exactly shiftRequirements[s] workers
-    // to each shift s
-    for (s = 0; s < nShifts; ++s) {
-      lhs = 0;
-      lhs += slacks[s];
+    // model.set(GRB_IntParam_Presolve, 0);
+    // model.set(GRB_DoubleParam_Heuristics, 0.5);
+    // model.set(GRB_IntParam_StartNodeLimit, 2000000000);
+    model.set(GRB_IntAttr_ModelSense, GRB_MAXIMIZE);
+    model.set(GRB_DoubleParam_TimeLimit, 0.1);
+    // model.set(GRB_IntParam_MIPFocus, 3);
+    // model.set(GRB_IntParam_SubMIPNodes, 10000);
+    // model.set(GRB_DoubleAttr_Start, GRB_UNDEFINED);
 
-      for (w = 0; w < nWorkers; ++w) {
-        lhs += x[w][s];
-      }
-
-      model.addConstr(lhs == shiftRequirements[s], Shifts[s]);
-    }
-
-    // Constraint: set totSlack equal to the total slack
-    lhs = 0;
-    for (s = 0; s < nShifts; ++s)
-    {
-      lhs += slacks[s];
-    }
-    model.addConstr(lhs == totSlack, "totSlack");
-
-    // Constraint: compute the total number of shifts for each worker
-    for (w = 0; w < nWorkers; ++w) {
-      lhs = 0;
-      for (s = 0; s < nShifts; ++s) {
-        lhs += x[w][s];
-      }
-      ostringstream vname;
-      vname << "totShifts" << Workers[w];
-      model.addConstr(lhs == totShifts[w], vname.str());
-    }
-
-    // Objective: minimize the total slack
-    GRBLinExpr obj = 0;
-    obj += totSlack;
-    model.setObjective(obj);
-
-    // Optimize
-    int status = solveAndPrint(model, totSlack, nWorkers, Workers, totShifts);
-    if (status != GRB_OPTIMAL)
-      return 1;
-
-    // Constrain the slack by setting its upper and lower bounds
-    totSlack.set(GRB_DoubleAttr_UB, totSlack.get(GRB_DoubleAttr_X));
-    totSlack.set(GRB_DoubleAttr_LB, totSlack.get(GRB_DoubleAttr_X));
-
-    // Variable to count the average number of shifts worked
-    GRBVar avgShifts =
-      model.addVar(0, GRB_INFINITY, 0, GRB_CONTINUOUS, "avgShifts");
-
-    // Variables to count the difference from average for each worker;
-    // note that these variables can take negative values.
-    diffShifts = model.addVars(nWorkers);
-    for (w = 0; w < nWorkers; ++w) {
-      ostringstream vname;
-      vname << Workers[w] << "Diff";
-      diffShifts[w].set(GRB_StringAttr_VarName, vname.str());
-      diffShifts[w].set(GRB_DoubleAttr_LB, -GRB_INFINITY);
-    }
-
-    // Constraint: compute the average number of shifts worked
-    lhs = 0;
-    for (w = 0; w < nWorkers; ++w) {
-      lhs += totShifts[w];
-    }
-    model.addConstr(lhs == nWorkers * avgShifts, "avgShifts");
-
-    // Constraint: compute the difference from the average number of shifts
-    for (w = 0; w < nWorkers; ++w) {
-      lhs = 0;
-      lhs += totShifts[w];
-      lhs -= avgShifts;
-      ostringstream vname;
-      vname << Workers[w] << "Diff";
-      model.addConstr(lhs == diffShifts[w], vname.str());
-    }
-
-    // Objective: minimize the sum of the square of the difference from the
-    // average number of shifts worked
-    GRBQuadExpr qobj;
-    for (w = 0; w < nWorkers; ++w) {
-      qobj += diffShifts[w] * diffShifts[w];
-    }
-    model.setObjective(qobj);
-
-    // Optimize
-    status = solveAndPrint(model, totSlack, nWorkers, Workers, totShifts);
-    if (status != GRB_OPTIMAL)
-      return 1;
-  }
-  catch (GRBException e) {
-    cout << "Error code = " << e.getErrorCode() << endl;
-    cout << e.getMessage() << endl;
-  }
-  catch (...) {
-    cout << "Exception during optimization" << endl;
-  }
-
-  for (int i = 0; i < xCt; ++i) {
-    delete[] x[i];
-  }
-  delete[] x;
-  delete[] slacks;
-  delete[] totShifts;
-  delete[] diffShifts;
-  delete env;
-
-  return 0;
-}
-
-int solveAndPrint(GRBModel& model,
-                  GRBVar&   totSlack,
-                  int       nWorkers,
-                  string*   Workers,
-                  GRBVar*   totShifts)
-{
-  model.optimize();
-  int status = model.get(GRB_IntAttr_Status);
+    model.optimize();
+      int status = model.get(GRB_IntAttr_Status);
 
   if ((status == GRB_INF_OR_UNBD) ||
       (status == GRB_INFEASIBLE)  ||
       (status == GRB_UNBOUNDED)     ) {
     cout << "The model cannot be solved " <<
     "because it is infeasible or unbounded" << endl;
-    return status;
   }
   if (status != GRB_OPTIMAL) {
     cout << "Optimization was stopped with status " << status << endl;
-    return status;
   }
 
-  // Print total slack and the number of shifts worked for each worker
-  cout << endl << "Total slack required: " <<
-    totSlack.get(GRB_DoubleAttr_X) << endl;
-  for (int w = 0; w < nWorkers; ++w) {
-    cout << Workers[w] << " worked " <<
-    totShifts[w].get(GRB_DoubleAttr_X) << " shifts" << endl;
-  }
-  cout << endl;
+    // // Create variables
+    // GRBVar x = model.addVar(0.0, 1.0, 0.0, GRB_BINARY, "x");
+    // GRBVar y = model.addVar(0.0, 1.0, 0.0, GRB_BINARY, "y");
+    // GRBVar z = model.addVar(0.0, 1.0, 0.0, GRB_BINARY, "z");
 
-  return status;
+    // // Set objective: maximize x + y + 2 z
+    // model.setObjective(x + y + 2 * z, GRB_MAXIMIZE);
+
+    // // Add constraint: x + 2 y + 3 z <= 4
+    // model.addConstr(x + 2 * y + 3 * z <= 4, "c0");
+
+    // // Add constraint: x + y >= 1
+    // model.addConstr(x + y >= 1, "c1");
+
+    // // Optimize model
+    // model.optimize();
+
+    // cout << x.get(GRB_StringAttr_VarName) << " "
+    //      << x.get(GRB_DoubleAttr_X) << endl;
+    // cout << y.get(GRB_StringAttr_VarName) << " "
+    //      << y.get(GRB_DoubleAttr_X) << endl;
+    // cout << z.get(GRB_StringAttr_VarName) << " "
+    //      << z.get(GRB_DoubleAttr_X) << endl;
+
+    // cout << "Obj: " << model.get(GRB_DoubleAttr_ObjVal) << endl;
+
+    // clockwork.printAssignments();
+    clockwork.printFifoObjective();
+
+    cout << "Obj: " << model.get(GRB_DoubleAttr_ObjVal) << endl;
+
+  } catch(GRBException e) {
+    cout << "Error code = " << e.getErrorCode() << endl;
+    cout << e.getMessage() << endl;
+  } catch(...) {
+    cout << "Exception during optimization" << endl;
+  }
+
+  return 0;
 }
