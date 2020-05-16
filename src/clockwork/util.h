b9e3ba77e956ef4605dff71594adc7d54540de4b
Arpan Gujarati <arpanbg@mpi-sws.org>
2020-05-16 07:30:55 +0200

    SlidingWindow implementation using Order Statistic Tree

--- src/clockwork/util.h
+++ src/clockwork/util.h
@@ -10,6 +10,10 @@
 #include <thread>
 #include <atomic>
 
+/* These two files are included for the Order Statistics Tree. */
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
 #define NUM_GPUS_1 1
 #define NUM_GPUS_2 2
 #define GPU_ID_0 0
@@ -90,6 +94,55 @@ class GPUClockState {
 
 };
 
+class SlidingWindow {
+private:
+	unsigned window_size;
+
+	/* An order statistics tree is used to implement a wrapper around a C++
+	   set with the ability to know the ordinal number of an item in the set
+	   and also to get an item by its ordinal number from the set.
+	   The data structure I use is implemented in STL but only for GNU C++.
+	   Some sources are documented below:
+	     -- https://gcc.gnu.org/onlinedocs/libstdc++/ext/pb_ds/
+	     -- https://codeforces.com/blog/entry/11080
+	     -- https://gcc.gnu.org/onlinedocs/libstdc++/ext/pb_ds/tree_based_containers.html
+	     -- https://opensource.apple.com/source/llvmgcc42/llvmgcc42-2336.9/libstdc++-v3/testsuite/ext/pb_ds/example/tree_order_statistics.cc.auto.html
+		 -- https://stackoverflow.com/questions/44238144/order-statistics-tree-using-gnu-pbds-for-multiset
+	     -- https://www.geeksforgeeks.org/order-statistic-tree-using-fenwick-tree-bit/ */
+
+	typedef __gnu_pbds::tree<
+		uint64_t,
+		__gnu_pbds::null_type,
+		std::less_equal<uint64_t>,
+		__gnu_pbds::rb_tree_tag,
+		__gnu_pbds::tree_order_statistics_node_update> OrderedMultiset;
+
+	/* We maintain a list of data items (FIFO ordered) so that the latest
+	   and the oldest items can be easily tracked for insertion and removal.
+	   And we also maintain a parallel OrderedMultiset data structure where the
+	   items are stored in an order statistics tree so that querying, say, the
+	   99th percentile value is easy. We also maintain an upper bound on sliding
+	   window size. After the first few iterations, the number of data items
+	   is always equal to the upper bound. Thus, we have:
+			-- Invariant 1: q.size() == oms.size()
+			-- Invariant 2: q.size() <= window_size */
+	std::list<uint64_t> q;
+	OrderedMultiset oms;
+
+public:
+	SlidingWindow() : window_size(100) {}
+	SlidingWindow(unsigned window_size) : window_size(window_size) {}
+
+	/* A simple test case */
+	static void test();
+
+	/* Assumption: q.size() == oms.size() */
+	unsigned get_size() { return q.size(); }
+
+	/* Requirement: rank < oms.size() */
+	uint64_t get_value(unsigned rank) { return (*(oms.find_by_order(rank))); }
+	void insert(uint64_t latest);
+};
 
 #define DEBUG_PRINT(msg) \
 	std::cout << __FILE__ << "::" << __LINE__ << "::" << __FUNCTION__ << " "; \
