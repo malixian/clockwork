ede4c438a4394de34e7e555006cb9acafc60fe5d
Jonathan Mace <jonathan.c.mace@gmail.com>
2020-08-24 15:24:40 +0200

    Add some command-line help to worker and worker dummy

--- src/worker.cpp
+++ src/worker.cpp
@@ -5,21 +5,42 @@
 #include "clockwork/worker.h"
 #include "clockwork/thread.h"
 
+void show_usage() {
+    std::stringstream s;
+    s << "USAGE:\n";
+    s << "  worker [OPTIONS]\n";
+    s << "DESCRIPTION\n";
+    s << "  Run a clockwork worker.  Will use all GPUs on this machine.\n";
+    s << "OPTIONS\n";
+    s << "  -h,  --help\n";
+    s << "        Print this message\n";
+    s << "  -c,  --config\n";
+    s << "        Specify a Clockwork config to use; otherwise the defaults will be used\n";
+    std::cout << s.str();
+}
 
 int main(int argc, char *argv[]) {
+	// Read args
+	std::string config_file_path;
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        if ((arg == "-h") || (arg == "--help")) {
+            show_usage();
+            return 0;
+        } else if ((arg == "-c") || (arg == "--config")) {
+        	config_file_path = argv[++i];
+        } else {
+        	std::cout << "Unknown option " << arg << std::endl;
+        	return 1;
+        }
+    }
+
+    // Init process
 	threading::initProcess();
 	util::setCudaFlags();
 	util::printCudaVersion();
 
 	std::cout << "Starting Clockwork Worker" << std::endl;
-
-	std::string config_file_path;
-
-	for (int i = 1; i < argc; i++){
-		
-		if ((strcmp(argv[i], "--config") == 0))
-			config_file_path = argv[++i];
-	}
 
 	ClockworkWorkerConfig config(config_file_path);
 
--- src/worker_dummy.cpp
+++ src/worker_dummy.cpp
@@ -4,36 +4,50 @@
 #include "clockwork/thread.h"
 #include <string>
 
+void show_usage() {
+    std::stringstream s;
+    s << "USAGE:\n";
+    s << "  worker [OPTIONS]\n";
+    s << "DESCRIPTION\n";
+    s << "  Run a clockwork worker.  Will use all GPUs on this machine.\n";
+    s << "OPTIONS\n";
+    s << "  -h,  --help\n";
+    s << "        Print this message\n";
+    s << "  -n,  --num_gpus\n";
+    s << "        The number of GPUs to emulate; defaults to 1\n";
+    s << "  -c,  --config\n";
+    s << "        Specify a Clockwork config to use; otherwise the defaults will be used\n";
+    std::cout << s.str();
+}
 
 int main(int argc, char *argv[]) {
-	//threading::initProcess();
-	//util::setCudaFlags();
-	//util::printCudaVersion();
-	//Don't need CUDA
+	// Read args
+	std::string config_file_path;
+	uint64_t num_gpus = 1;
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        if ((arg == "-h") || (arg == "--help")) {
+            show_usage();
+            return 0;
+        } else if ((arg == "-c") || (arg == "--config")) {
+        	config_file_path = argv[++i];
+        } else if ((arg == "-n") || (arg == "--num_gpus")) {
+        	num_gpus = std::stoul(argv[++i]);
+        } else {
+        	std::cout << "Unknown option " << arg << std::endl;
+        	return 1;
+        }
+    }
 
 	std::cout << "Starting Clockwork Worker" << std::endl;
 
-	std::string config_file_path;
-	std::string num_gpus = "2";//default
-
-	for (int i = 1; i < argc; i++){
-		
-		if ((strcmp(argv[i], "--config") == 0))
-			config_file_path = argv[++i];
-
-		if ((strcmp(argv[i], "--num_gpus") == 0))
-			num_gpus = argv[++i];
-	}
-
 	ClockworkWorkerConfig config(config_file_path);
-	config.num_gpus = std::stoul(num_gpus,nullptr,0);
+	config.num_gpus = num_gpus;
 
 	clockwork::ClockworkDummyWorker* clockwork = new clockwork::ClockworkDummyWorker(config);
-	//clockwork::ClockworkDummyWorker* clockwork = new clockwork::ClockworkDummyWorker();
 	clockwork::network::worker::Server* server = new clockwork::network::worker::Server(clockwork);
 	clockwork->setController(server);
 
-	//threading::setDefaultPriority(); // Revert thread priority
 	clockwork->join();
 
 	std::cout << "Clockwork Worker Exiting" << std::endl;
